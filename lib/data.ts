45e4b608fbcf2de73ffe3b9e1b811ceeca80cfa1
Soham Gupta <soham.gupta003@gmail.com>
2024-04-27 11:46:49 +0530

    fix: Chrome Ext Project URL

--- lib/data.ts
+++ lib/data.ts
@@ -65,7 +65,7 @@ export const projectsData = [
       "A Chrome extension that fetches live scores and other match details for the Indian Premier League (IPL) cricket matches using the API provided by cricketdata.org.",
     tags: ["API", "Javascript", "CSS", "JSON"],
     imageUrl: chromeExtImg,
-    link: "https://github.com/gupta-soham/ipl-chrome-extension/https://github.com/gupta-soham/ipl-chrome-extension/",
+    link: "https://github.com/gupta-soham/ipl-chrome-extension/",
   },
 ] as const;
 
